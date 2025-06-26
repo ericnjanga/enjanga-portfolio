ab73f93073b15c8462e6fff728efa4a79295cb50
Eric Njanga <eric.njanga@gmail.com>
2025-06-26 17:07:09 -0400

    chore: update component mock data

--- src/app/home/ContentAbout.tsx
+++ src/app/home/ContentAbout.tsx
@@ -4,15 +4,27 @@ import { CustomTile } from 'enjanga-next-3-components-lib'; // ENJ NPM component
 const ContentAbout = () => (
   <Grid className="">
     <Column lg={{ span: 5, offset: 11 }} md={4} sm={4} className="">
-      <CustomTile title="title" text="component text ..." />
+      <CustomTile
+        iconName="Leadership"
+        title="title"
+        text="component text ..."
+      />
     </Column>
     <Column lg={{ span: 10, offset: 6 }} md={4} sm={4} className="">
       <Grid className="">
         <Column lg={5} md={4} sm={4}>
-          <CustomTile title="title" text="component text ..." />
+          <CustomTile
+            iconName="App Developer"
+            title="title"
+            text="component text ..."
+          />
         </Column>
         <Column lg={5} md={4} sm={4}>
-          <CustomTile title="title" text="component text ..." />
+          <CustomTile
+            iconName="Hills"
+            title="title"
+            text="component text ..."
+          />
         </Column>
       </Grid>
     </Column>
--- src/app/home/ContentBestWork.tsx
+++ src/app/home/ContentBestWork.tsx
@@ -4,7 +4,11 @@ import { CustomTile } from 'enjanga-next-3-components-lib'; // ENJ NPM component
 const ContentBestWork = () => (
   <Grid className="tabs-group-content">
     <Column lg={16} md={8} sm={4} className="landing-page__tab-content">
-      <CustomTile title="title" text="component text ..." />
+      <CustomTile
+        stackOrder="horizontal"
+        title="Biscuit tootsie roll fruitcake gummies marshmallow bear"
+        text="Marzipan halvah topping chocolate bonbon chocolate cake cupcake jujubes. Soufflé tiramisu gummies brownie bonbon. Dragée lemon drops jelly-o powder marzipan chocolate cake candy canes pastry. Tiramisu apple pie halvah tootsie roll apple pie."
+      />
     </Column>
   </Grid>
 );
