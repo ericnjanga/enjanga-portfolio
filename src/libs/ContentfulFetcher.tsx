724ca0293b109053716b80c7f5b83ab00235ea36
Eric Njanga <eric.njanga@gmail.com>
2025-07-25 16:17:18 -0400

    chore: create a content fetcher component

--- src/libs/ContentfulFetcher.tsx
+++ src/libs/ContentfulFetcher.tsx
@@ -1,31 +1,102 @@
-import React, { createContext, useContext, useState, ReactNode } from 'react';
+import React, {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  ReactNode,
+} from 'react';
+import { useContentful } from '@/hooks/useContentful';
+import { queryData } from './queries';
 
 interface ContentContextValue {
+  id?: string;
   title: string;
   blurb?: string;
   description: string;
 }
 
-// Create the context
+// Creating CMS data's wrapping context
 const ContentContext = createContext<ContentContextValue | undefined>(
   undefined
 );
 
 // Provider Component with Render Props support
 interface ContentfulFetcherProps {
+  dataFor: 'Landing Page Banner' | 'Best work'; // Describe which type of content must be fetched
   children: (props: ContentContextValue) => ReactNode;
 }
 
 export const ContentfulFetcher: React.FC<ContentfulFetcherProps> = ({
+  dataFor,
   children,
 }) => {
-  const [title] = useState('Hello from Context');
-  const [blurb] = useState('Marzipan cake I love shortbread lollipop.');
-  const [description] = useState(
-    'Soufflé marshmallow I love cheesecake bonbon. Toffee macaroon croissant macaroon sweet roll. Jelly brownie ice cream wafer sugar plum macaroon. Marzipan cake I love shortbread lollipop.'
-  );
+  /**
+   * Fetching ContentFul data in all languages, and handling errors and loading time
+   * ----------------------
+   */
+  // Getting the currently active locale...
+  const activeLang = 'en-CA'; // useContext(LanguageContext);
+  let paramInUse = {
+    query: '',
+    variables: { locale1: 'en-CA', locale2: 'fr', sectionId: '' },
+    queryKey: '',
+    trackingInfo: '',
+  };
 
-  const value: ContentContextValue = { title, blurb, description };
+  // ...
+  switch (dataFor) {
+    case 'Landing Page Banner':
+      paramInUse.trackingInfo = 'Landing Page Banner';
+      paramInUse.query = queryData.infoBlockById;
+      paramInUse.variables = {
+        ...paramInUse.variables,
+        sectionId: '4llAs4gW4mc1fikxbW6u4V',
+      };
+      break;
+
+    case 'Best work':
+      paramInUse.trackingInfo = 'Best work';
+      paramInUse.query = queryData.projectById;
+      paramInUse.variables = {
+        ...paramInUse.variables,
+        sectionId: '5y2JSha3mykWdGkUf6XcQp',
+      };
+      break;
+  }
+
+  // ...
+  const { data, isLoading, error } = useContentful({
+    query: paramInUse.query,
+    variables: paramInUse.variables,
+    // variables: { sectionId: id, locale1: "en-CA", locale2: "fr" },
+    queryKey: paramInUse.queryKey,
+    // queryKey: `pageSection-${id}`,
+    infoTracking: paramInUse.trackingInfo,
+  });
+
+  // ...
+  const id = data?.en?.sys?.id ?? '';
+  const title = data?.en?.title ?? '';
+  const blurb = data?.en?.blurb ?? '';
+  const description =
+    data?.en?.description?.json?.content[0]?.content[0]?.value ?? '';
+
+  if (paramInUse.trackingInfo === 'Best work') {
+    console.log('>>>>>>> data?.en?.sys?.id = ', data?.en?.sys?.id);
+  }
+
+  // // Display a placeholder is there is no modal context or the data fetching is not yet completed
+  // if (isLoading) {
+  //   return <Preloader />;
+  // }
+  // // Display an error messaye if there was problem fetching data
+  // if (error) return <p>{t("ErrorLoadingPosts")}</p>;
+  /**
+   * Fetching ContentFul data in all languages, and handling errors and loading time
+   * ----------------------
+   */
+
+  const value: ContentContextValue = { id, title, blurb, description };
 
   return <>{children(value)}</>;
 };
