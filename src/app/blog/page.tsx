51aaafafcb47e223c49d1fd84f813b0767786173
Eric Njanga <eric.njanga@gmail.com>
2025-08-05 11:38:21 -0400

    chore: fetch blog posts in the blog landing page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,8 +1,9 @@
 'use client';
 
 import Link from 'next/link';
-import { Banner } from 'enjanga-next-3-components-lib'; // ENJ NPM component library
+import { Banner, CustomTile } from 'enjanga-next-3-components-lib'; // ENJ NPM component library
 import { Grid, Column } from '@carbon/react';
+import { ContentfulFetcher } from '@/libs/ContentfulFetcher';
 
 export default function BlogRoot() {
   const mockData = {
@@ -15,13 +16,24 @@ export default function BlogRoot() {
     <>
       <Banner title={mockData.banner.title} />
       <Grid fullWidth>
-        <Column lg={16} md={8} sm={4} className="landing-page__banner">
-          <p>
-            <Link href="/blog/article-page" passHref legacyBehavior>
-              Article page
-            </Link>
-          </p>
-        </Column>
+        <ContentfulFetcher dataFor="List of Blog Posts">
+          {({ orderedItems }) =>
+            orderedItems?.map((item) => {
+              console.log('======', item);
+              return (
+                <Column
+                  key={item.sys.id}
+                  lg={5}
+                  md={4}
+                  sm={4}
+                  className="landing-page__banner"
+                >
+                  <CustomTile title={item.title} />
+                </Column>
+              );
+            })
+          }
+        </ContentfulFetcher>
       </Grid>
     </>
   );
