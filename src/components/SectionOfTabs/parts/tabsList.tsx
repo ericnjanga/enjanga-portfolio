291bf9deef906de7753e05812b04da1d8148b9f1
Eric Njanga <eric.njanga@gmail.com>
2025-09-03 04:34:20 -0400

    chore: upgrading tabs

--- src/components/SectionOfTabs/parts/tabsList.tsx
+++ src/components/SectionOfTabs/parts/tabsList.tsx
@@ -1,11 +1,26 @@
+import {
+  Code,
+  LogoFigma,
+  IbmIbv,
+  Collaborate,
+  Platforms,
+} from '@carbon/icons-react';
 import { Tab, TabList } from '@carbon/react';
-import { User, Settings, Code, Dashboard } from '@carbon/icons-react';
 import { InformationBlock } from '@/libs/CMS-content-types';
 
 interface SectionTabsListProps {
   className?: string;
   listOfItems?: InformationBlock[];
 }
+
+// map string keys from CMS to actual icon components
+const iconMap: Record<string, React.ComponentType> = {
+  Code,
+  LogoFigma,
+  IbmIbv,
+  Collaborate,
+  Platforms,
+};
 
 const SectionTabsList = ({ listOfItems, className }: SectionTabsListProps) => {
   if (!listOfItems) {
@@ -14,11 +29,24 @@ const SectionTabsList = ({ listOfItems, className }: SectionTabsListProps) => {
 
   return (
     <TabList aria-label="List of expertises" className={className}>
-      {listOfItems?.map((item) => (
-        <Tab key={item.sys.id} renderIcon={Dashboard}>
-          {item.title}
-        </Tab>
-      ))}
+      {listOfItems?.map((item) => {
+        const IconComponent = iconMap[item?.icon || ''] || Code; // fallback to Code if not found
+
+        /**
+         * TODO:
+         * 1) I've created a temporary solution to reduce item.title string size by striping anything after "&"
+         * but this is just a temporary patch.
+         * WHat should be done:
+         * 1) Creating a "shortTitle" field inside "InformationBlock" to render shorter titles
+         */
+        // remove any substring that starts with "& "
+        let cleanTitle = item.title.replace(/&\s.*$/, '').trim();
+        return (
+          <Tab key={item.sys.id} renderIcon={IconComponent}>
+            {cleanTitle}
+          </Tab>
+        );
+      })}
     </TabList>
   );
 };
