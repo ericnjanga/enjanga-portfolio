f4adb4d7dd7e7351292609c22643f7a5d9b2a74d
Eric Njanga <eric.njanga@gmail.com>
2025-09-05 10:41:06 -0400

    chore: adding color tokens

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -8,6 +8,7 @@ import SectionOfTabs from '../../components/SectionOfTabs/index';
 import ContentAbout from './ContentAbout';
 import ContentBestWork from './ContentBestWork';
 import './../home/_home.scss';
+import './../home/_home-theming.scss';
 import { ContentfulFetcher } from '@/libs/ContentfulFetcher';
 import { BackgroundSection } from '@/components/BackgroundSection';
 import { cmsContentIds } from '@/libs/CMS-references';
