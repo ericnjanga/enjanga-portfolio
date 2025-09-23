9eae2710e040f715a126bea44a6e9d24523ea771
Eric Njanga <eric.njanga@gmail.com>
2025-09-23 16:57:01 -0400

    chore: refactor contentful fetcher function

--- src/libs/ContentfulFetcher.tsx
+++ src/libs/ContentfulFetcher.tsx
@@ -1,17 +1,13 @@
-import React, {
-  createContext,
-  useContext,
-  useState,
-  useEffect,
-  ReactNode,
-} from 'react';
+// src/libs/ContentfulFetcher.tsx
+'use client';
+
+import React, { createContext, useContext, ReactNode } from 'react';
 import { useContentful } from '@/hooks/useContentful';
-import { queryData } from './CMS-content-queries';
-import { cmsContentIds } from './CMS-references';
 import type { Node } from '@contentful/rich-text-types';
 import { InformationBlock } from './CMS-content-types';
 import { CQ_quote_propsType } from 'enjanga-components-library';
 import { sortByOrderProp } from '@utils/helpers';
+import { getQueryConfig, DataFor } from './fetchEntries';
 
 interface ContentContextValue {
   id?: string;
@@ -24,32 +20,17 @@ interface ContentContextValue {
   };
   plainDescription?: string;
   richDescription?: { json: { content: Node[] } };
-  items?: InformationBlock[]; // Unordered list of items
-  orderedItems?: InformationBlock[] | CQ_quote_propsType[]; // Ordered list of items
+  items?: InformationBlock[];
+  orderedItems?: InformationBlock[] | CQ_quote_propsType[];
 }
 
-// Creating CMS data's wrapping context
 const ContentContext = createContext<ContentContextValue | undefined>(
   undefined
 );
 
-// Provider Component with Render Props support
 interface ContentfulFetcherProps {
-  dataFor: // Describe which type of content must be fetched
-  | 'Landing Page Banner'
-    | 'Blog Page Banner'
-    | 'Footer Copyright'
-    | 'Single Work'
-    | 'Single Blog Post'
-    | 'InfoBlock by parentId' 
-    | 'List of Best Work'
-    | 'List of quotes'
-    | 'List of Scope of expertise'
-    | 'List of Blog Posts'
-    | 'List of About Info'
-    | 'List of Footer Links';
-
-  contentId?: string; // ...
+  dataFor: DataFor;
+  contentId?: string;
   children: (props: ContentContextValue) => ReactNode;
 }
 
@@ -58,146 +39,18 @@ export const ContentfulFetcher: React.FC<ContentfulFetcherProps> = ({
   contentId = '',
   children,
 }) => {
-  /**
-   * Fetching ContentFul data in all languages, and handling errors and loading time
-   * ----------------------
-   */
-  // Getting the currently active locale...
-  const activeLang = 'en-CA'; // useContext(LanguageContext);
-  let paramInUse = {
-    query: '',
-    variables: {
-      locale1: 'en-CA',
-      locale2: 'fr',
-      sectionId: '',
-      parentRefId: '',
-    },
-    queryKey: '',
-    trackingInfo: '',
-  };
-  // ...
+  const { query, variables, trackingInfo } = getQueryConfig(dataFor, contentId);
+
+  const { data, isLoading, error } = useContentful({
+    query,
+    variables,
+    queryKey: `${trackingInfo}-${contentId}`,
+    infoTracking: trackingInfo,
+  });
+
   let plainDescription;
   let richDescription;
 
-  // ...
-  switch (dataFor) {
-    case 'Landing Page Banner':
-      paramInUse.trackingInfo = 'Landing Page Banner';
-      paramInUse.query = queryData.infoBlockById;
-      paramInUse.variables = {
-        ...paramInUse.variables,
-        sectionId: cmsContentIds.categories['Landing Page Banner'],
-      };
-      break;
-
-    case 'Blog Page Banner':
-      paramInUse.trackingInfo = 'Blog Page Banner';
-      paramInUse.query = queryData.infoBlockById;
-      paramInUse.variables = {
-        ...paramInUse.variables,
-        sectionId: cmsContentIds.categories['Blog Page Banner'],
-      };
-      break;
-
-    case 'Footer Copyright':
-      paramInUse.trackingInfo = 'Footer Copyright';
-      paramInUse.query = queryData.infoBlockById;
-      paramInUse.variables = {
-        ...paramInUse.variables,
-        sectionId: cmsContentIds.categories['Footer Copyright'],
-      };
-      break;
-
-    case 'Single Work':
-      paramInUse.trackingInfo = 'Single Work';
-      paramInUse.query = queryData.projectById;
-      paramInUse.variables = {
-        ...paramInUse.variables,
-        sectionId: contentId,
-      };
-      break;
-
-    case 'Single Blog Post':
-      paramInUse.trackingInfo = 'Single Blog Post';
-      paramInUse.query = queryData.blogPostById;
-      paramInUse.variables = {
-        ...paramInUse.variables,
-        sectionId: contentId,
-      };
-      break;
-
-    case 'InfoBlock by parentId':
-      paramInUse.trackingInfo = 'InfoBlock by parentId';
-      paramInUse.query = queryData.infoBlockByParentCollection;
-      paramInUse.variables = {
-        ...paramInUse.variables,
-        parentRefId: contentId,
-      };
-      break;
-
-    case 'List of Best Work':
-      paramInUse.trackingInfo = 'List of Best Work';
-      paramInUse.query = queryData.projectsCollection;
-      paramInUse.variables = {
-        ...paramInUse.variables,
-      };
-      break;
-
-    case 'List of quotes':
-      paramInUse.trackingInfo = 'List of quotes';
-      paramInUse.query = queryData.quotesCollection;
-      paramInUse.variables = {
-        ...paramInUse.variables,
-      };
-      break;
-
-    case 'List of Scope of expertise':
-      paramInUse.trackingInfo = 'List of Scope of expertise';
-      paramInUse.query = queryData.infoBlockByParentCollection;
-      paramInUse.variables = {
-        ...paramInUse.variables,
-        parentRefId: cmsContentIds.categories['Scope of expertise'],
-      };
-      break;
-
-    case 'List of About Info':
-      paramInUse.trackingInfo = 'List of About Info';
-      paramInUse.query = queryData.infoBlockByParentCollection;
-      paramInUse.variables = {
-        ...paramInUse.variables,
-        parentRefId: cmsContentIds.categories['About section'],
-      };
-      break;
-
-    case 'List of Footer Links':
-      paramInUse.trackingInfo = 'List of Footer Links';
-      paramInUse.query = queryData.infoBlockByParentCollection;
-      paramInUse.variables = {
-        ...paramInUse.variables,
-        parentRefId: cmsContentIds.categories['Footer section'],
-      };
-      break;
-
-    case 'List of Blog Posts':
-      paramInUse.trackingInfo = 'List of Blog Posts';
-      paramInUse.query = queryData.blogPostCollection;
-      paramInUse.variables = {
-        ...paramInUse.variables,
-      };
-      break;
-  }
-
-  // ...
-  const { data, isLoading, error } = useContentful({
-    query: paramInUse.query,
-    variables: paramInUse.variables,
-    // variables: { sectionId: id, locale1: "en-CA", locale2: "fr" },
-    queryKey: paramInUse.queryKey,
-    // queryKey: `pageSection-${id}`,
-    infoTracking: paramInUse.trackingInfo,
-  });
-
-  // ...
   switch (dataFor) {
     case 'Landing Page Banner':
     case 'Blog Page Banner':
@@ -208,7 +61,6 @@ export const ContentfulFetcher: React.FC<ContentfulFetcherProps> = ({
       break;
   }
 
-  // ...
   const id = data?.en?.sys?.id ?? '';
   const title = data?.en?.title ?? '';
   const blurb = data?.en?.blurb ?? '';
@@ -216,14 +68,7 @@ export const ContentfulFetcher: React.FC<ContentfulFetcherProps> = ({
   const items = data?.en?.items;
   let orderedItems;
 
-  // TODO: DELETE AFTER OPTIMIZATION
-  // ---------------
-  if (dataFor === 'Blog Page Banner') {
-    console.log(`************* data: `, data);
-  }
-
-  // Sort the list of items by the "order" property (item.order)
-  switch (paramInUse.trackingInfo) {
+  switch (trackingInfo) {
     case 'List of Scope of expertise':
     case 'InfoBlock by parentId':
     case 'List of Blog Posts':
@@ -231,32 +76,9 @@ export const ContentfulFetcher: React.FC<ContentfulFetcherProps> = ({
     case 'List of quotes':
     case 'List of About Info':
     case 'List of Footer Links':
-      // // TODO: DELETE AFTER OPTIMIZATION
-      // // ---------------
-      // if (paramInUse.trackingInfo==='List of quotes') {
-      //   console.log('>>>[1111]>>>> data = ', data);
-      //   console.log('>>>[****]>>>> data?.en?.items = ', data?.en?.items);
-      // }
       orderedItems = sortByOrderProp(items);
-      break; 
+      break;
   }
-
-  // // Display a placeholder is there is no modal context or the data fetching is not yet completed
-  // if (isLoading) {
-  //   return <Preloader />;
-  // }
-  // // Display an error messaye if there was problem fetching data
-  // if (error) return <p>{t("ErrorLoadingPosts")}</p>;
-  /**
-   * Fetching ContentFul data in all languages, and handling errors and loading time
-   * ----------------------
-   */
-
-  // TODO: DELETE AFTER OPTIMIZATION
-  // ---------------
-  // if (paramInUse.trackingInfo === 'Single Work') {
-  //   console.log(`.....title=${title}`);
-  // }
 
   const value: ContentContextValue = {
     id,
@@ -272,7 +94,6 @@ export const ContentfulFetcher: React.FC<ContentfulFetcherProps> = ({
   return <>{children(value)}</>;
 };
 
-// Custom hook for child components if needed
 export const useContent = () => {
   const context = useContext(ContentContext);
   if (!context) {
