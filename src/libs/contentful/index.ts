030724677cd04d747708a5fe3979786710f3bb2c
Eric Njanga <eric.njanga@gmail.com>
2025-09-30 08:09:38 -0400

    chore: create a database helper class

---
+++ src/libs/contentful/index.ts
@@ -0,0 +1,18 @@
+/**
+ * enjanga-database:
+ * ----------------
+ * 
+ * 
+ * 
+ * 
+ * NOTE: This class will be renamed from "Contentful" to "enjanga-database"
+ */
+
+
+class EnjangaDatabase {
+  constructor() {
+
+  }
+}
+
+export default EnjangaDatabase;
