84ab8148d8e8e330a959d7aa073c7e8b25871942
Eric Njanga <eric.njanga@gmail.com>
2026-07-26 21:10:09 -0400

    update: fetching old posts

--- src/libs/contentful/contentful-queryConfig.ts
+++ src/libs/contentful/contentful-queryConfig.ts
@@ -70,7 +70,7 @@ export function getContentfulQueryConfig(dataFor: DataFor1 | DataFor2 | DataFor3
 
     case 'Service Entry Collection':
       query = queryData.serviceCollection;
-      variables.limit = 1000;
+      variables.limit = 5;
       variables.skip = 0;
       break;
 
